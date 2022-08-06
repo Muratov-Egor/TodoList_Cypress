6ab6d2fdfd708a33157db854ceab3d3b807c35d6
egormuratov <egor_muratov@hotmail.com>
2022-08-06 16:19:28 +0700

    test example

--- cypress/e2e/login_tests.cy.js
+++ cypress/e2e/login_tests.cy.js
@@ -1,7 +1,33 @@
+import {allureTestInfo} from '../utils/AllureHelper'
+import {LoginForm} from '../elements/LoginForm'
+import {MessageSnackBar} from '../elements/MessageSnackBar'
+Cypress.on('uncaught:exception', () => {
+  return false
+})
 describe('Tests login page', () => {
-  it('Login page', () => {
-    cy.allure().step('Open login page', () => {
-      cy.visit('/')
+  beforeEach(() => {
+    allureTestInfo({
+      suite: 'Login Page',
+      epic: 'Authorisation',
+      tags: ['login', 'regress', 'smoke'],
+      owner: 'Egor Muratov'
     })
+  })
+
+  it('Login', () => {
+    const loginForm = new LoginForm()
+    const messageSnackBar = new MessageSnackBar()
+
+    cy.openPage('Open login page', '/')
+    messageSnackBar
+      .messageBlockIsNotExist()
+    loginForm
+      .fillInEmail('email@email.com')
+      .fillInPassword('pass')
+      .clickLoginButton()
+    messageSnackBar
+      .messageBlockHasText('User not found')
+      .clickButtonCloseMessage()
+      .messageBlockIsNotExist()
   })
 })
