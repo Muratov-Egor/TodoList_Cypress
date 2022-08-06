3832227121ec59eca7b359f2539d5848f6ae607c
egormuratov <egor_muratov@hotmail.com>
2022-08-06 15:18:13 +0700

    open Page command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,30 +1,7 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
+import {allureStep} from '../utils/AllureHelper'
+
 Cypress.Commands.add('openPage', (title, url) => {
-  cy.allure().step(title, () => {
+  allureStep(title, () => {
     cy.visit(url)
   })
 })
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
