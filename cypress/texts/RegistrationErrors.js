9247aacf1577095a9b0dd36dc71d128ae78e6e57
egormuratov <egor_muratov@hotmail.com>
2022-08-21 15:37:57 +0700

    registration tests

---
+++ cypress/texts/RegistrationErrors.js
@@ -0,0 +1,6 @@
+export const RegistrationErrors = {
+  userExists: 'Such a user already exists',
+  emailIsEmpty: 'Incorrect email',
+  wrongPassword: 'Minimum password length 6 characters',
+  userCreated: 'User created'
+}
