51e57510fceb176b555814b31d41980fb3e27a22
egormuratov <egor_muratov@hotmail.com>
2022-08-27 16:36:01 +0700

    added the loginRequest command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,21 @@
 import {allureStep} from '../utils/AllureHelper'
+import {User} from '../models'
 
 Cypress.Commands.add('openPage', (title, url) => {
   allureStep(title, () => {
     cy.visit(url)
+  })
+})
+
+Cypress.Commands.add('loginRequest', () => {
+  cy.request(
+    'POST',
+    '/api/auth/login',
+    {...new User()}
+  ).then((res) => {
+    window.localStorage.setItem('userData', JSON.stringify({
+      token: res.body.token,
+      userId: res.body.userId,
+    }))
   })
 })
